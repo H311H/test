eeeb3300b32c885068b8617a31cc325900ac965b
HansHedz <60444453+H311H@users.noreply.github.com>
2022-12-09 10:13:02 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='test',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/H311H/test.git',
+    author='Hans Hernandez',
+    author_email='hans.hernandez@digitalbankscorp.com',
+    license='unlicense',
+    packages=['test'],
+    zip_safe=False
+)
